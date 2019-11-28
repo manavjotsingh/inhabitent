3f8c18d39c9b22c2825c0de11d9bf58428ea7d33
Manav Singh <manavjot13@gmail.com>
2019-11-27 21:39:58 -0800

    added journal css

--- themes/Inhabitent/functions.php
+++ themes/Inhabitent/functions.php
@@ -111,3 +111,20 @@ function inhabitent_remove_submenus() {
     remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
 }
 add_action( 'admin_menu', 'inhabitent_remove_submenus', 110 );
+
+/**
+* Removes width and height attributes from image tags
+*
+* @param string $html
+*
+* @return string
+*/
+function remove_image_size_attributes( $html ) {
+	return preg_replace( '/(width|height)="\d*"/', '', $html );
+	}
+	
+	// Remove image size attributes from post thumbnails
+	add_filter( 'post_thumbnail_html', 'remove_image_size_attributes' );
+	
+	// Remove image size attributes from images added to a WordPress post
+	add_filter( 'image_send_to_editor', 'remove_image_size_attributes' );
